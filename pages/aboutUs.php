ff1025b5b5a7d241eead62d8512460c10bf0c031
Tobias Schipper <tobias.schipper@student.nhlstenden.com>
2024-10-20 17:10:12 +0200

    Title aangepast dat het bij de pagina hoort en css voor de review pagina aangemaakt

--- pages/aboutUs.php
+++ pages/aboutUs.php
@@ -14,7 +14,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Circle-Solution</title>
+    <title>Circle-Solution - About Us</title>
     <link rel="stylesheet" href="../css/aboutUs.css" type="text/css">
 </head>
 <body>
