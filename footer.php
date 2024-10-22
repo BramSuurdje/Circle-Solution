d9330d4430bd43de1eb37400bd3420513bb152f8
Thijs Arkes <thijs.arkes@nhlstenden.student.com>
2024-10-22 13:05:26 +0200

    added footer and pakkets

---
+++ footer.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Circle-Solution</title>
+    <link rel="stylesheet" href="footer.css" type="text/css">
+</head>
+
+<body>
+    
+   <div class="gridContainer">
+        <div class="footerTop">
+            <div class="footerMenu">
+                <div class="tekstA">
+                    <ul>
+                     <li>Home</li>
+                    </ul>
+                </div>
+                <div class="tekstA">
+                    <ul>
+                     <li>Diensten</li>
+                    </ul>
+                </div>
+                <div class="tekstA">  
+                    <ul>
+                     <li>Tarieven</li>
+                    </ul>
+                </div>
+                <div class="tekstA">
+                    <ul>
+                     <li>FAQ</li>
+                    </ul>
+                </div>
+                <div class="tekstA">
+                    <ul>
+                     <li>Contacten</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="FooterKfoto">
+                <div class="kleineFoto"><img src="Circle-solution/facebookLogoWithOutBorder.svg" alt=""></div>
+                <div  class="kleineFoto"><img src="Circle-solution/Instagram.svg" alt=""></div>
+                <div  class="kleineFoto"><img src="Circle-Solution/xLogo.svg" alt=""></div>
+            </div>
+        </div>
+     <div><hr></div>
+
+     <div class="footerBottom">
+         <div class="tekstC"><p>2024. All rights reserved</p></div>
+         <div class="bigBoy"><img src="Circle-solution/CS-logo-dark (1).svg" alt=""></div>
+         <div class="tekstD">
+            <ul>
+                <p>Terms of service</p></div>
+            </ul>
+         <div class="tekstD">
+            <ul>
+             <p>Privacy Policy</p>
+            </ul> 
+        </div>  
+     </div>
+   
+     </div> 
+
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,14 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Circle-Solution</title>
-    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="stYle.css" type="text/css">
 </head>
 <body>
-    <!-- jasper was here -->
-     <!-- Laat Makelele lekker de tyfus krijgen -->
+    <!-- hallo -->
+    <!-- menu wouter -->
+
+    <!-- pakketen thijs -->
 
 
 
-    <!--Tobias Was hier -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <div class="flexContainer"></div>
+     <div class="BlokA Blue">
+         <div class="Mgekozen"><p>Meest gekozen</p></div>
+         <div class="bundelA">
+            <p>
+               1-50 gebruikers €180,00 per maand 51-500 gebruikers €799,00 per maand
+            </p>
+         </div>
+     </div>
+     <div class="BlokB Dblue"></div>
+     <div class="BlokC Dblue"></div>
+     <div class="BlokD Blue">
+         <div class="bundleB"></div>
+     </div>
+
+
+   
 </body>
 </html>
