45f86e2744b5f8b934519eb8ae8bf116b8f83b07
Jasper-Wesseling <wesselingjasper@hotmail.com>
2024-10-22 14:11:58 +0200

    added the fonts to all the pages

--- pages/aboutUs.php
+++ pages/aboutUs.php
@@ -15,6 +15,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Circle-Solution - About Us</title>
+    <link rel="stylesheet" href="../css/style.css" type="text/css">
     <link rel="stylesheet" href="../css/aboutUs.css" type="text/css">
 </head>
 <body>
--- pages/contact.php
+++ pages/contact.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Circle-Solution</title>
+    <link rel="stylesheet" href="../css/style.css" type="text/css">
     <link rel="stylesheet" href="../css/contact.css" type="text/css">
-    <link rel="stylesheet" href="../style.css" type="text/css">
 </head>
 <body>
     <section class="contact">
