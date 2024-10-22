68aac703ae6056f24449a8df9c832dc62b41a6e2
Jasper-Wesseling <wesselingjasper@hotmail.com>
2024-10-22 10:35:32 +0200

    made the contact section added the form and style

--- pages/contact.php
+++ pages/contact.php
@@ -8,8 +8,6 @@
     <link rel="stylesheet" href="../style.css" type="text/css">
 </head>
 <body>
-    <!-- hallo -->
-    <!-- jasper -->
     <section class="contact">
         <div class="contactLeft">
             <h2 class="contactLeftTitle">Laten we samen de punten verbinden</h2>
@@ -19,11 +17,11 @@
                         <a href="mailto:Voorbeeld@voorbeeld.nl">Voorbeeld@voorbeeld.nl</a>
                     </li>
                     <li class="infoListItem">
-                        <img src="../svg/telephone.svg" alt="emailSVG"/>
+                        <img src="../svg/telephone.svg" alt="telephoneSVG"/>
                         <a href="tel:123456789">+123456789</a>
                     </li>
                     <li class="infoListItem">
-                        <img src="../svg/pin.svg" alt="emailSVG"/>
+                        <img src="../svg/pin.svg" alt="pinSVG"/>
                         <a href="https://maps.app.goo.gl/iSLbz94wwd9JqAM37" target="_blank">Van Schaikweg 94, 7811 KL Emmen</a>
                     </li>
                 </lu>
@@ -42,25 +40,38 @@
         <div class="contactRight">
             <p class="contactRightText">Ik ben geïnteresseerd in...</p>
             <form action="<?php $_SERVER["PHP_SELF"] ?>" method="POST">
-                <div>
-                    <label for="vlees">Vlees:</label>
-                    <input type="checkbox" name="recept[]" value="vlees" id="vlees">
-                    <label for="vis">Vis:</label>
-                    <input type="checkbox" name="recept[]" value="vis" id="vis">
-                    <label for="vegetarisch">Vegetarisch:</label>
-                    <input type="checkbox" name="recept[]" value="vegetarisch" id="vegetarisch">
-                    <label for="vegan">Vegan:</label>
-                    <input type="checkbox" name="checkbox" value="vegan" id="vegan">
-                    <label for="kapsalon">Kapsalon:</label>
-                    <input type="checkbox" name="recept[]" value="kapsalon" id="kapsalon">
-                </div>
-                <label for="vnaam">Voornaam:</label>
-                <input type="text" name="voornaam" id="vnaam">
-                <label for="anaam">Achternaam:</label>
-                <input type="text" name="achternaam" id="anaam">
-                <label for="email">Email:</label>
-                <input type="text" name="email" id="email">
-                <input class="submit" type="submit" value="Verzenden">
+                <div class="FormIntrestContainer">
+                    <input class="checkboxIntrest" type="radio" id="app-management" name="interest" value="App management">
+                    <label for="app-management" class="checkboxIntrestLable">App management</label>
+
+                    <input class="checkboxIntrest" type="radio" id="communication" name="interest" value="Communication">
+                    <label for="communication" class="checkboxIntrestLable">Communication</label>
+
+                    <input class="checkboxIntrest" type="radio" id="information" name="interest" value="Information">
+                    <label for="information" class="checkboxIntrestLable">information</label>
+
+                    <input class="checkboxIntrest" type="radio" id="document-management" name="interest" value="Document management">
+                    <label for="document-management" class="checkboxIntrestLable">Document management</label>
+                </div> 
+                <input class="contactInputField" type="text" required placeholder="Voornaam" name="voornaam" id="vnaam">
+                <input class="contactInputField" type="text" required placeholder="Achternaam" name="achternaam" id="anaam">
+                <input class="contactInputField" type="text" required placeholder="Email" name="email" id="email">
+                <?php 
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                        $intrest = filter_input(INPUT_POST, "interest");
+                        if ($intrest  === null) {
+                            $intrest = "algemeen";
+                        }
+                        $firstName = filter_input(INPUT_POST, "voornaam");
+                        $lastName = filter_input(INPUT_POST, "achternaam");
+                        $email = filter_input(INPUT_POST, "email");
+                        echo("<p class='contactFieldResponse'>Bedankt voor u bericht ".$firstName." ".$lastName."! Wij sturen u email over het geselecteerde interesse: ".$intrest." naar dit email adres: ".$email."</p>");
+                    }
+                ?>
+                <button class="submit" id="submit" type="submit">
+                    <img class="submitIcon" src="../svg/verstuur.svg" alt="emailSVG"/>
+                    Verstuur bericht
+                </button>
             </form>
         </div>
     </section>
