a3d4b7100013cb8abff6882557478e48f226ba52
WWesseling <wesselingwouter@hotmail.com>
2024-10-24 16:51:09 +0200

    fixed the navbar on home page

--- header.php
+++ header.php
@@ -5,8 +5,9 @@
 
     <header>
         <div class="logo">
-            <img src="../Circle-Solution/img/CS-logo-light.svg" alt="Circle Solutions Logo">
+            <img src="svg/CS-logo-light.svg" alt="Circle Solutions Logo">
         </div>
+        
         <nav class="nav">
             <ul>
                 <li><a href="index.php" class="<?= ($current_page == 'index.php') ? 'active' : ''; ?>">Home</a></li>
@@ -17,4 +18,18 @@
                 <li><a href="pages/contact.php" class="<?= ($current_page == 'contact.php') ? 'active' : ''; ?>">Contact</a></li>
             </ul>
         </nav>
+
+        <script>
+            window.addEventListener('scroll', function() {
+                var header = document.querySelector('header');
+                if (window.scrollY > 80) {
+                    header.classList.add('scrolled');
+                } else {
+                    header.classList.remove('scrolled');
+                }
+            });
+        </script>
+
     </header>
+
+    
